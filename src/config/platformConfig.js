03724ae13d8d55e5841a1d23720eb6480fb15ae8
Arjunbir Singh <arjunbirsingh1699@gmail.com>
2025-12-01 06:09:38 +0530

    Add new configuration module

---
+++ src/config/platformConfig.js
@@ -0,0 +1,16 @@
+// Platform usernames and configuration
+export const PLATFORM_CONFIG = {
+  leetcode: {
+    username: 'arjunbirsingh1699',
+    profileUrl: 'https://leetcode.com/u/arjunbirsingh1699/',
+  },
+  github: {
+    username: 'Arjun140205',
+    profileUrl: 'https://github.com/Arjun140205',
+  },
+};
+
+// Total problems available on each platform (approximate)
+export const PLATFORM_TOTALS = {
+  leetcode: 3000,
+};
